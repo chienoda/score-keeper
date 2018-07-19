5fa363358ed073b3137264689b5b733bfc020803
Chie Noda <chienoda119@gmail.com>
2018-07-18 21:14:01 -0400

    Add .winner class

--- js/main.js
+++ js/main.js
@@ -11,12 +11,12 @@ var gameOver = false;
 var winningScore = 5;
 
 
-winningScore.textContent = winningScore;
 
 p1Button.addEventListener("click", function() {
 	if(!gameOver) {
 		p1Score++;
 		if(p1Score === winningScore) {
+			p1Display.classList.add("winner");
 			gameOver = true;
 		}
  		p1Display.textContent = p1Score;
@@ -27,11 +27,18 @@ p2Button.addEventListener("click", function() {
 	if(!gameOver) {
 		p2Score++;
 		if(p2Score === winningScore) {
+			p2Display.classList.add("winner");
 			gameOver = true;
 		}
 		p2Display.textContent = p2Score;
 	}
 });
+
+// winningScoreInput.addEventListener('change', function() {
+// 	label.innerHTML = this.value;
+// });
+
+
 
 
 
